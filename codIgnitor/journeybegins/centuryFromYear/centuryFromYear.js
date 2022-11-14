88f2c476a660cfa4a2d9cbcd459d7c72da4e46bc
thunder-godd <iammonytony8@gmail.com>
2022-11-14 21:09:02 +0300

    renamed some files

--- codIgnitor/journeybegins/centuryFromYear/centuryFromYear.js
+++
@@ -1,5 +0,0 @@
-function solution(year) {
-	let century = Math.ceil(year / 100);
-
-	return century;
-}
---
+++ codesignal/journeybegins/centuryFromYear/centuryFromYear.ts
@@ -0,0 +1,5 @@
+function solution2(year: number) {
+	const century: number = Math.ceil(year / 100);
+
+	return century;
+}
