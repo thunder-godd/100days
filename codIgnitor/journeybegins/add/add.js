99cff0dd21d8f3ebb33712e645b91fd2459fb4fd
thunder-godd <iammonytony8@gmail.com>
2022-11-14 20:30:47 +0300

    added git ignore,package.json,switched to typescript

--- codIgnitor/journeybegins/add/add.js
+++
@@ -1,3 +0,0 @@
-function solution(param1, param2) {
-	return param1 + param2;
-}
---
+++ codIgnitor/journeybegins/add/add.ts
@@ -0,0 +1,6 @@
+function solution(param1: number, param2: number): number {
+	return param1 + param2;
+}
+//Test cases
+//
+//1 : solution(1, 2);
