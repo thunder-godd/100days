7074a3a7c7a9fbfece0e57fe6bc613cecce4f4a9
thunder-godd <iammonytony8@gmail.com>
2022-11-15 08:44:37 +0300

    adjacentElementsProduct solved

---
+++ codesignal/EdgeOfTheOcean/adjacentElementsProduct.ts
@@ -0,0 +1,10 @@
+function solution(inputArray: number[]): number {
+	const len: number = inputArray.length;
+	let prods: number[] = [];
+	for (let i = 0; i < len - 1; i++) {
+		let prod: number = inputArray[i] * inputArray[i + 1];
+		prods = [...prods, prod];
+	}
+	console.log(prods);
+	return Math.max(...prods);
+}
